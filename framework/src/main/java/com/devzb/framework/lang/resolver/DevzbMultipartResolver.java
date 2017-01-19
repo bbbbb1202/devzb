9c8685a4f8b7d1eedc63151ee78f44443bd3bd9c
zhangbin <zhangbin@kakasure.com>
2017-01-19 14:49:00 +0800

    commons fileupload 重写判定Multipart

---
+++ framework/src/main/java/com/devzb/framework/lang/resolver/DevzbMultipartResolver.java
@@ -0,0 +1,36 @@
+
+package com.devzb.framework.lang.resolver;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+public class DevzbMultipartResolver extends CommonsMultipartResolver {
+
+	private Logger			logger	= LoggerFactory.getLogger(getClass());
+	private List<String>	exclusions;
+
+	@Override
+	public boolean isMultipart(javax.servlet.http.HttpServletRequest request) {
+
+		String path = request.getServletPath();
+		if (path.indexOf("/resources") == 0) {// 静态文件直接跳过
+			return false;
+		}
+
+		logger.info(path);
+
+		if (exclusions.contains(path)) {
+			return false;
+		}
+
+		return super.isMultipart(request);
+	}
+
+	public void setExclusions(List<String> exclusions) {
+		logger.info("MultipartResolver exclusions url: {}", exclusions.toString());
+		this.exclusions = exclusions;
+	}
+}
