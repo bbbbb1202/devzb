9c10ed06774ae1aea43631ccb4a56163c0563752
zhangbin <zhangbin@kakasure.com>
2017-03-15 15:26:49 +0800

    静态文件地址以配置方式设置

--- framework/src/main/java/com/devzb/framework/lang/resolver/DevzbMultipartResolver.java
+++ framework/src/main/java/com/devzb/framework/lang/resolver/DevzbMultipartResolver.java
@@ -7,13 +7,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+/**
+ * 判定是否是Multipart请求以及设置staticHost
+ * 
+ * @author zhangbin
+ *
+ */
 public class DevzbMultipartResolver extends CommonsMultipartResolver {
 
-	private Logger			logger	= LoggerFactory.getLogger(getClass());
+	private Logger			logger		= LoggerFactory.getLogger(getClass());
+	/**
+	 * 需要排除的url
+	 */
 	private List<String>	exclusions;
+	/**
+	 * 静态文件服务器地址
+	 */
+	private String			staticHost	= "//static.devzb.com";
 
 	@Override
 	public boolean isMultipart(javax.servlet.http.HttpServletRequest request) {
+		request.setAttribute("staticHost", staticHost);
 
 		String path = request.getServletPath();
 		if ("/favicon.ico".equals(path) || path.indexOf("/resources") == 0) {// 静态文件直接跳过
@@ -33,4 +47,10 @@ public class DevzbMultipartResolver extends CommonsMultipartResolver {
 		logger.info("MultipartResolver exclusions url: {}", exclusions.toString());
 		this.exclusions = exclusions;
 	}
+
+	public void setStaticHost(String staticHost) {
+		logger.info("MultipartResolver staticHost: {}", staticHost);
+		this.staticHost = staticHost;
+	}
+
 }
--- self/src/main/webapp/WEB-INF/views/common/head.jsp
+++ self/src/main/webapp/WEB-INF/views/common/head.jsp
@@ -2,8 +2,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<meta name="_csrf" content="${_csrf.token}"/>
 	<meta name="_csrf_header" content="${_csrf.headerName}"/>
-	<!-- 新 Bootstrap 核心 CSS 文件 -->
 	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="/resources/css/common.css" />
-	<script type="text/javascript" src="http://static.devzb.com/resources/jquery/1.11/jquery.js"></script>
-	<script type="text/javascript" src="http://static.devzb.com/resources/layer/layer.js"></script>
+	<script type="text/javascript" src="${staticHost}/resources/jquery/1.11/jquery.js"></script>
+	<script type="text/javascript" src="${staticHost}/resources/layer/layer.js"></script>
