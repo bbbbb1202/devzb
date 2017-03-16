0d0c8964c313f00784f50698bebd77f216fb528c
zhangbin <zhangbin@kakasure.com>
2017-03-16 16:33:11 +0800

    开发模式jetty添加了SSL

--- self/src/test/java/com/devzb/main/SelfStart.java
+++ self/src/test/java/com/devzb/main/SelfStart.java
@@ -1,9 +1,14 @@
 package com.devzb.main;
 
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.xml.XmlConfiguration;
 
 /**
  * 启动类
@@ -13,31 +18,59 @@ import org.eclipse.jetty.xml.XmlConfiguration;
  */
 public class SelfStart {
 
-	private static boolean isHttps = false;
 	public static void main(String[] args) throws Exception {
 
-		System.out.println(isHttps);
+		Server server = new Server();
 
-		Server server = new Server(9999);
-		XmlConfiguration.main(args);
-		ServletContextHandler context = new WebAppContext("src/main/webapp", "/");
-		server.setHandler(context);
+		HttpConfiguration http_config = new HttpConfiguration();
+		http_config.setSecureScheme("https");
+		http_config.setSecurePort(8443);
+		http_config.setOutputBufferSize(32768);
+
+		ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
+		http.setPort(8080);
+		http.setIdleTimeout(1000 * 60 * 60);
+
+		server.addConnector(http);
+
+		Resource keystore = Resource.newClassPathResource("/thekeystore");
+		if (keystore != null && keystore.exists()) {// 判定是否启用8443端口
+			SslContextFactory sslContextFactory = new SslContextFactory();
+			sslContextFactory.setKeyStoreResource(keystore);
+			sslContextFactory.setKeyStorePassword("caodao1688");
+			sslContextFactory.setKeyManagerPassword("caodao1688");
+
+			HttpConfiguration https_config = new HttpConfiguration(http_config);
+			https_config.addCustomizer(new SecureRequestCustomizer());
+
+			ServerConnector https = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(https_config));
+			https.setPort(8443);
+			https.setIdleTimeout(500000);
+
+			server.addConnector(https);
+			System.out.println("SSL access to the examples has been enabled on port 8443");
+			System.out.println("You can access the application using SSL on https://localhost:8443");
+			System.out.println();
+		}
+
+		WebAppContext bb = new WebAppContext();
+		bb.setServer(server);
+		bb.setContextPath("/");
+		bb.setWar("src/main/webapp");
+
+		// 如果要测试JSESSIONID，开启下面的注释
+		// ((AbstractSessionManager)
+		// bb.getSessionHandler().getSessionManager()).setUsingCookies(false);
+
+		server.setHandler(bb);
 
 		try {
-			System.out.println(">>>>>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
 			server.start();
-			System.in.read();
-			System.out.println(">>>>>> STOPPING EMBEDDED JETTY SERVER");
-			while (System.in.available() == 0) {
-				Thread.sleep(5000);
-			}
-			server.stop();
 			server.join();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(100);
 		}
-
 	}
 
 }
