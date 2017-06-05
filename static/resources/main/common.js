a67bfbc8771e8a2925f1c4f4cc3666340d77192b
zhangbin <bbbbb1202@163.com>
2017-06-05 13:44:16 +0800

    common.js add function getNowDay

--- static/resources/main/common.js
+++ static/resources/main/common.js
@@ -244,7 +244,9 @@ function getFormatDate(date, pattern) {
     }
     return date.format(pattern);
 }
-Date.prototype.format = function (format) {
+
+
+Date.prototype.format = function(format) {
     var o = {
         "M+": this.getMonth() + 1,
         "d+": this.getDate(),
@@ -255,14 +257,27 @@ Date.prototype.format = function (format) {
         "S": this.getMilliseconds()
     }
     if (/(y+)/.test(format)) {
-        format = format.replace(RegExp.$1, (this.getFullYear() + "")
-            .substr(4 - RegExp.$1.length));
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     }
     for (var k in o) {
         if (new RegExp("(" + k + ")").test(format)) {
-            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k]
-                : ("00" + o[k]).substr(("" + o[k]).length));
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
         }
     }
     return format;
 };
+
+/**
+ * 获得日期
+ * @param num
+ * 整数，0或者不传值则为当天，1为后一天，-1为前一天
+ * @returns
+ */
+function getNowDay(num) {
+    if (typeof num == 'undefined' || num == undefined || num == null || typeof num != 'number') {
+        num = 0;
+    }
+    var now = new Date();
+    now.setDate(now.getDate() + num);
+    return now.format('MM月dd日');//格式要改的话就改这里
+}
